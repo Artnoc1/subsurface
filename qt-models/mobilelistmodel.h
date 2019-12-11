34f3e9bfe3d2694cda75a95d675eea5135b65f94
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2019-12-11 10:37:50 +0100

    mobile/divelist: create memory management class for models
    Since we want to add a second model, but not have to manage two
    models everywhere, create a class MobileModels that contains
    both of the models. When calling reset() on that class, it will
    reset both of the models, etc.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- qt-models/mobilelistmodel.h
+++ qt-models/mobilelistmodel.h
@@ -44,9 +44,7 @@ public:
 		FirstGasRole,
 		SelectedRole,
 	};
-	MobileListModel();
-	static MobileListModel *instance();
-	void resetModel(DiveTripModelBase::Layout layout);	// Switch between tree and list view
+	MobileListModel(DiveTripModelBase *source);
 	void expand(int row);
 	void unexpand();
 	void toggle(int row);
@@ -58,7 +56,6 @@ private:
 		bool visible;
 		int first, last;
 	};
-	void connectSignals();
 	std::vector<IndexRange> rangeStack;
 	QModelIndex sourceIndex(int row, int col, int parentRow = -1) const;
 	int numSubItems() const;
@@ -81,6 +78,7 @@ private:
 	QHash<int, QByteArray> roleNames() const override;
 	int shown() const;
 
+	DiveTripModelBase *source;
 	int expandedRow;
 private slots:
 	void prepareRemove(const QModelIndex &parent, int first, int last);
@@ -92,6 +90,19 @@ private slots:
 	void changed(const QModelIndex &topLeft, const QModelIndex &bottomRight, const QVector<int> &roles);
 };
 
+// This convenience class provides access to the two mobile models.
+// Moreover, it provides an interface to the source trip-model.
+class MobileModels {
+public:
+	static MobileModels *instance();
+	MobileListModel *listModel();
+	void clear(); // Clear all dive data
+	void reset(); // Reset model after having reloaded the core data
+private:
+	MobileModels();
+	DiveTripModelTree source;
+	MobileListModel lm;
+};
 
 // Helper functions - these are actually defined in DiveObjectHelper.cpp. Why declare them here?
 QString formatSac(const dive *d);
