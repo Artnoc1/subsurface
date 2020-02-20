7a8233d12b990aa22583a29e2824cb368ed491c6
Dirk Hohndel <dirk@hohndel.org>
2020-02-20 13:22:57 -0800

    dive models: add helper role to find trip above or below dive
    This is only used in the mobile UI where the sort direction is fixed and we
    refer to dives based on the tree model. So the terms used and the concepts
    that these rely on should be guaranteed to be valid.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- qt-models/mobilelistmodel.cpp
+++ qt-models/mobilelistmodel.cpp
@@ -46,6 +46,8 @@ QHash<int, QByteArray> MobileListModelBase::roleNames() const
 	roles[FirstGasRole] = "firstGas";
 	roles[SelectedRole] = "selected";
 	roles[DiveInTripRole] = "diveInTrip";
+	roles[TripAbove] = "tripAbove";
+	roles[TripBelow] = "tripBelow";
 	return roles;
 }
 
