5fe75e84750286b9f860f99fb9734a531faf221e
Dirk Hohndel <dirk@hohndel.org>
2020-02-21 21:31:31 -0800

    mobile models: add trip location and notes
    First step towards making those accessible and then editable.
    
    Signed-off-by: Dirk Hohndel <dirk@hohndel.org>

--- qt-models/mobilelistmodel.cpp
+++ qt-models/mobilelistmodel.cpp
@@ -48,6 +48,8 @@ QHash<int, QByteArray> MobileListModelBase::roleNames() const
 	roles[DiveInTripRole] = "diveInTrip";
 	roles[TripAbove] = "tripAbove";
 	roles[TripBelow] = "tripBelow";
+	roles[TripLocationRole] = "tripLocation";
+	roles[TripNotesRole] = "tripNotes";
 	return roles;
 }
 
